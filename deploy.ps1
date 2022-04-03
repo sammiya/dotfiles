859d3edfc583e3334da156a54c9ea10ade6db462
sammiya <338@tutanota.de>
2022-04-03 12:21:36 +0900

    wip

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,18 @@
+# 予め管理者権限で以下を実行しておく:
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
 mkdir -f $env:USERPROFILE\Documents\WindowsPowerShell
-New-Item -Force -ItemType SymbolicLink -Value $PSScriptRoot\profile.ps1 -Path $env:USERPROFILE\Documents\WindowsPowerShell
+New-Item -Force -ItemType SymbolicLink -Value $PSScriptRoot\profile.ps1 -Path $env:USERPROFILE\Documents\WindowsPowerShell\profile.ps1
+New-Item -Force -ItemType SymbolicLink -Value $PSScriptRoot\settings.json -Path $env:APPDATA\Code\User\settings.json
+
+# winget export で出力されるファイルを利用することも考えたが、
+# VSCode が出力されなかったりよくわからない挙動があるのでふつうにコマンドを列挙
+
+winget install vim.vim
+rm $env:PUBLIC\Desktop\gVim*.lnk
+winget install git.git
+winget install Microsoft.VisualStudioCode
+winget install Discord.Discord
+winget install Google.Chrome
+
+cat extensions.txt | % { code --install-extension $_ }
--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,11 @@
 $newPath = @(
   "$env:PROGRAMFILES\Git\cmd"
+  "$env:PROGRAMFILES\Vim\vim82"
 ) -join ";"
 
 $env:PATH = $newPath + ';' + $env:PATH
+
+# "PS " のプレフィックスを消す
+function prompt {
+  $(get-location) + "> "
+}
